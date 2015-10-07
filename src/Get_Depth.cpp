6ae195a8545a4aa49a2736c25243178fe6160e9b
Ankush Chatterjee <ankushchatterjee.iitkgp@gmail.com>
2015-10-07 23:38:41 +0530

    Color isolation and noise reduction

--- src/Get_Depth.cpp
+++ src/Get_Depth.cpp
@@ -16,6 +16,14 @@ class getDepth
   cv::Mat left_img;
   cv::Mat_<uint8_t> disp;
 
+  int LowerH;
+  int LowerS;
+  int LowerV;
+  int UpperH;
+  int UpperS;
+  int UpperV;
+  int erosion_kernel;
+
 public:
   getDepth()
     : it_(nh_)
@@ -23,9 +31,25 @@ public:
     image_sub_l = it_.subscribe("/stereo/left/image_rect_color", 1, &getDepth::left_callback, this);
     image_sub_d = nh_.subscribe("/stereo/disparity", 1 ,&getDepth::disparity_callback, this);
 
+
+    LowerH = 0;
+    LowerS = 0;
+    LowerV = 0;
+    UpperH = 180;
+    UpperS = 196;
+    UpperV = 170;
+    erosion_kernel = 3;
     cv::namedWindow("left");
     cv::namedWindow("disparity");
-    //cv::namedWindow("mask");
+    cv::namedWindow("mask");
+    cv::createTrackbar("LowerH","left",&LowerH,180,NULL);
+    cv::createTrackbar("UpperH","left",&UpperH,180,NULL);
+    cv::createTrackbar("LowerS","left",&LowerS,256,NULL);
+    cv::createTrackbar("UpperS","left",&UpperS,256,NULL);
+    cv::createTrackbar("LowerV","left",&LowerV,256,NULL);
+    cv::createTrackbar("UpperV","left",&UpperV,256,NULL);
+    cv::createTrackbar("ErosionKernel 2n+3","left",&erosion_kernel,6,NULL);
+
     //cv::namedWindow("masked_disparity")
   }
 
@@ -33,14 +57,91 @@ public:
   {
     cv::destroyWindow("left");
     cv::destroyWindow("disparity");
-    //cv::destroyWindow("mask");
+    cv::destroyWindow("mask");
     //cv::destroyWindow("masked_disparity")
   }
 
   void left_callback(const sensor_msgs::ImageConstPtr&);
   void disparity_callback(const stereo_msgs::DisparityImagePtr&);
+  cv::Mat IsolateColor(const cv::Mat& src);
+  cv::Mat reduceNoise(const cv::Mat& src);
 };
 
+
+cv::Mat getDepth::IsolateColor(const cv::Mat& src)
+{
+    cv::Mat out,img_hsv; 
+    cv::cvtColor(src,img_hsv,CV_BGR2HSV);
+    cv::inRange(img_hsv,cv::Scalar(LowerH,LowerS,LowerV),cv::Scalar(UpperH,UpperS,UpperV),out);
+    return out; 
+}
+
+cv::Mat getDepth::reduceNoise(const cv::Mat& src)
+{
+    cv::Mat dst = src.clone();
+    int i, j, k, l, count;
+
+    //-------- Remove Borders ----------//
+    for(i=0;i<dst.rows;i++)
+    {
+        dst.at<uchar>(i,0)=0;
+        dst.at<uchar>(i,dst.cols-1)=0;
+    }
+    for(i=0;i<dst.cols;i++)
+    {
+        dst.at<uchar>(0,i)=0;
+        dst.at<uchar>(dst.rows-1,i)=0;
+    }
+    //----------------------------------//
+
+    //-------- Perform a variation of Erosion followed by Dilation -----------//
+    int kern = 2*erosion_kernel + 3;
+    for(i=1;i<src.rows-1;i++)
+    {
+        for(j=1;j<src.cols-1;j++)
+        {
+            if(src.at<uchar>(i,j)==255)
+            {
+                count=0;
+                 for(l=i-kern/2;l<=i+kern/2;l++)
+                 {
+                    for(k=j-kern/2;k<=j+kern/2;k++)
+                        {
+                            if((!(i==l&&j==k)) && src.at<uchar>(l,k)==255)
+                            {
+                                count ++;
+                            }
+                        }
+                 }
+                 if(count<=(kern*kern)/2) dst.at<uchar>(i,j)=0;
+            }
+
+        }
+    }
+     for(i=1;i<src.rows-1;i++)
+    {
+        for(j=1;j<src.cols-1;j++)
+        {
+            if(src.at<uchar>(i,j)==0)
+            {
+                count=0;
+                 for(l=i-kern/2;l<=i+kern/2;l++)
+                 {
+                    for(k=j-kern/2;k<=j+kern/2;k++)
+                        {
+                            if((!(i==l&&j==k)) && src.at<uchar>(l,k)==0)
+                            {
+                                count ++;
+                            }
+                        }
+                 }
+                 if(count<=(kern*kern)/2) dst.at<uchar>(i,j)=255;
+            }
+
+        }
+    }
+    return dst;
+}
 
 void getDepth::left_callback(const sensor_msgs::ImageConstPtr& msg)
   {
@@ -63,9 +164,14 @@ void getDepth::disparity_callback(const stereo_msgs::DisparityImagePtr& msg)
     cv::Mat_<float> dMat(msg->image.height, msg->image.width, reinterpret_cast<float*>(&(msg->image.data[0])));
     this->disp = (dMat) * (255/msg->max_disparity);
 
+    cv::Mat img_mask1 = IsolateColor(this->left_img);
+    cv::Mat img_mask2 = reduceNoise(img_mask1);
+
     cv::imshow("disparity",disp);
+    cv::imshow("mask",img_mask2);
     cv::waitKey(30);
   }  
+
 
 int main(int argc, char** argv)
 {
